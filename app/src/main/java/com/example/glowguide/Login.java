87b66036bd435c7ce44ad9dbebd270e6ff45a6f8
Tehilalev <Tehilalev44@gmail.com>
2024-06-16 22:01:18 +0300

    reset password + edit profile 3

--- app/src/main/java/com/example/glowguide/Login.java
+++ app/src/main/java/com/example/glowguide/Login.java
@@ -15,12 +15,9 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
--- app/src/main/java/com/example/glowguide/UserProfileActivity.java
+++ app/src/main/java/com/example/glowguide/UserProfileActivity.java
@@ -3,7 +3,6 @@ package com.example.glowguide;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
